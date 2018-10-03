359cbf2c019008cfb4f3efbaadf5ecf292705114
Ubuntu <ubuntu@ip-172-31-7-120.us-west-2.compute.internal>
2018-10-03 20:09:23 +0000

    made partition_testtrain cleaner

--- bundlenet.py
+++ bundlenet.py
@@ -14,8 +14,11 @@ from nibabel.streamlines import load as load_trk
 import dipy.tracking.utils as dtu
 
 from sklearn.metrics import cohen_kappa_score, jaccard_similarity_score
+from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
+
+import seaborn as sns
 
 
 def read_sl(fname):
@@ -38,7 +41,7 @@ def process_sl(streamlines_tract,take_n_sl,vol_shape,size):
         vol[sl[0], sl[1], sl[2]] = 1
         p0 = resize(np.max(vol, 0),(vol_shape[0], vol_shape[0]))
         p1 = resize(np.max(vol, 1),(vol_shape[0], vol_shape[0]))
-        p2 = np.max(vol, 2) 
+        p2 = resize(np.max(vol, 2),(vol_shape[0], vol_shape[0])) 
         projected = np.concatenate((p0,p1,p2))
         projected = morph.binary_dilation(projected, iterations=5)
         projected = resize(projected, (size, size,1)) #expects 3-d, like rgb
@@ -46,34 +49,14 @@ def process_sl(streamlines_tract,take_n_sl,vol_shape,size):
     return projected_all
 
 def partition_testtrain(test_perc, val_perc, streamlines_processed):
-    take_n_sl = streamlines_processed[0].shape[0]
-    take_n_bundles = len(streamlines_processed)
-    size_slimage = streamlines_processed[0].shape[1]
-    sl_fortrain = int(round(take_n_sl*(1-test_perc-val_perc)))
-    sl_fortest = int(round(take_n_sl*(test_perc)))
-    sl_forval = int(round(take_n_sl*(val_perc)))
-
-    
-    data_train = np.zeros((np.int(sl_fortrain*take_n_bundles),
-                           size_slimage,size_slimage,1), dtype='float32')
-    data_test = np.zeros((np.int(sl_fortest*take_n_bundles),
-                           size_slimage,size_slimage,1), dtype='float32')
-    data_val = np.zeros((np.int(sl_forval*take_n_bundles),
-                           size_slimage,size_slimage,1), dtype='float32')
-    labels_train= np.zeros(np.int(sl_fortrain*take_n_bundles), dtype='float32')
-    labels_test= np.zeros(np.int(sl_fortest*take_n_bundles), dtype='float32')
-    labels_val= np.zeros(np.int(sl_forval*take_n_bundles), dtype='float32')
-
-                      
-    for sidx, s in enumerate(streamlines_processed):
-        data_train[sidx*sl_fortrain:(sidx+1)*sl_fortrain,:,:,:] = s[0:sl_fortrain]
-        data_test[sidx*sl_fortest:(sidx+1)*sl_fortest,:,:,:] = s[sl_fortrain:sl_fortrain+sl_fortest]
-        data_val[sidx*sl_forval:(sidx+1)*sl_forval,:,:,:] = s[sl_fortrain+sl_fortest:sl_fortrain+sl_fortest+sl_forval]
-
-        labels_train[sidx*sl_fortrain:(sidx+1)*sl_fortrain]=sidx
-        labels_test[sidx*sl_fortest:(sidx+1)*sl_fortest]=sidx
-        labels_val[sidx*sl_forval:(sidx+1)*sl_forval]=sidx
-    return (data_train, data_test, data_val labels_train, labels_test, labels_val)
+    all_streamlines = streamlines_processed[0]
+    all_labels = np.zeros((streamlines_processed[0].shape[0]))
+    for i in range(1,len(streamlines_processed)):
+        all_streamlines = np.concatenate((all_streamlines,streamlines_processed[i]),axis=0)
+        all_labels = np.concatenate((all_labels,i*np.zeros((streamlines_processed[i].shape[0]))))
+    data_trainval, data_test, labels_trainval, labels_test = train_test_split(all_streamlines, all_labels, test_size=test_perc, stratify=all_labels)
+    data_train, data_val, labels_train, labels_val = train_test_split(data_trainval, labels_trainval, test_size=val_perc/test_perc, stratify=labels_trainval)
+    return (data_train, data_test, data_val, labels_train, labels_test, labels_val)
 
 def plot_accuracy(training):
     accuracy = training.history['acc']
@@ -93,9 +76,26 @@ def print_accuarcystats(p,labels_test,bundle_names):
     p_bundles = np.array([bundle_names[ii] for ii in p_idx])
     loaded_from_file = np.load('./subject1_bundles.npz')
     labels_test = loaded_from_file['labels_test']
-    actual_labels = np.array([bundle_names[ii] for ii in (labels_test - 1).astype(int)])
+    actual_labels = np.array([bundle_names[ii] for ii in (labels_test).astype(int)])
     print("Percent correct is %s " % np.mean(p_bundles == actual_labels))
     kappa = cohen_kappa_score(p_bundles, actual_labels)
     print("Kappa is: %s" % kappa)
     jaccard = jaccard_similarity_score(p_bundles, actual_labels)
     print("Jaccard is: %s" % jaccard)
+    return p_bundles, actual_labels
+
+def plotconfusionmat(bundle_names,p_bundles, actual_labels):
+    confusion = np.zeros((len(bundle_names), len(bundle_names)))
+    arr_bundle_names = np.array(bundle_names)
+    for xx in range(len(p_bundles)):
+        idx1 = np.where(arr_bundle_names == p_bundles[xx])
+        idx2 = np.where(arr_bundle_names == actual_labels[xx])
+        n = np.sum(actual_labels == actual_labels[xx])
+        confusion[idx1, idx2] += 1 / n
+    sort_idx = np.argsort(arr_bundle_names)
+    fig, ax = plt.subplots(1)
+    sns.heatmap(confusion[sort_idx][:, sort_idx],
+            xticklabels=arr_bundle_names[sort_idx], 
+            yticklabels=arr_bundle_names[sort_idx], ax=ax)
+    fig.set_size_inches([10, 8])
+    
